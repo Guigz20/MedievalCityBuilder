e00a3560a6fdeb62c0ea80cbaefe77891de809eb
Guillaume <guillaume.m.carron@gmail.com>
2025-09-02 23:58:04 +0200

    Essais d'optimisation d'affichage de la map

--- Scripts/Map/map.py
+++ Scripts/Map/map.py
@@ -1,5 +1,7 @@
 import pygame
 import pygame as pg
+from pygame.examples.grid import TILE_SIZE
+
 from Scripts.Utilz.constants import *
 import random
 
@@ -11,17 +13,20 @@ class Map:
         self.map = {}
         self.width, self.height = 0, 0
         self.offset = [0, 0]
+        self.blit_offset = [-self.tile_size[0]*INITIAL_SCALE_FACTOR, -self.tile_size[1]*INITIAL_SCALE_FACTOR]
 
         self.initialize()
-        self.surface = self.redraw_surface(INITIAL_SCALE_FACTOR)
+
+        self.in_screen_tiles = {}
+        self.reset_in_game_tiles(INITIAL_SCALE_FACTOR)
 
         self.should_move = False
         self.direction = ""
-        self.speed = 40
+        self.speed = 120
 
     def initialize(self):
-        for x in range(0, SCREEN_WIDTH//(self.tile_size[0]*INITIAL_SCALE_FACTOR) + 1):
-            for y in range(0, SCREEN_HEIGHT//(self.tile_size[0]*INITIAL_SCALE_FACTOR) + 1):
+        for x in range(0, 200):
+            for y in range(0, 200):
                 surface = pg.image.load('../Assets/Map/Images/Grass01.png')
                 surface = pg.transform.scale(surface,(self.tile_size[0]*INITIAL_SCALE_FACTOR, self.tile_size[1]*INITIAL_SCALE_FACTOR))
                 if random.randint(0, 1) == 0:
@@ -33,22 +38,38 @@ class Map:
                 self.height = x
         print(SCREEN_WIDTH//(self.tile_size[0]*INITIAL_SCALE_FACTOR))
 
-    def redraw_surface(self, scale):
-        surface = pg.surface.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+    def reset_in_game_tiles(self, scale):
+        print("Resetting in-game tiles")
+        is_first_tile = True
         for tile in self.map:
-            surface.blit(self.map[tile], (tile[0]*self.tile_size[0]*scale, tile[1]*self.tile_size[0]*scale))
-        return surface
+            tile_pos_x, tile_pos_y = tile[0]*self.tile_size[0]*scale, tile[1]*self.tile_size[1]*scale
+            tile_size = self.tile_size[0]*scale
+            if tile_pos_x > self.offset[0]-tile_size and tile_pos_y > self.offset[1]-tile_size:
+                if tile_pos_x < self.offset[0]+tile_size+SCREEN_WIDTH and tile_pos_y < self.offset[1]+tile_size+SCREEN_HEIGHT:
+                    self.in_screen_tiles[tile] = self.map[tile]
+                    if is_first_tile:
+                        self.blit_offset = [tile[0]*scale*self.tile_size[0] + self.offset[0], tile[1]*scale*self.tile_size[0] + self.offset[1]]
+                        is_first_tile = False
 
-    def draw(self):
-        self.screen.blit(self.surface, (self.offset[0], self.offset[1]))
+    def draw(self,scale):
+        for tile in self.in_screen_tiles:
+            self.screen.blit(self.in_screen_tiles[tile], (tile[0]*scale*self.tile_size[0] + self.offset[0], tile[1]*scale*self.tile_size[0] + self.offset[1]))
+        if abs(self.blit_offset[0]) >= self.tile_size[0]*scale or abs(self.blit_offset[1]) >= self.tile_size[0]*scale:
+            self.reset_in_game_tiles(scale)
+            print(self.blit_offset)
 
     def move(self, dt: int):
-        if self.should_move:
-            if self.direction == "right":
-                self.offset[0] -= self.speed*dt
-            elif self.direction == "left":
-                self.offset[0] += self.speed*dt
-            elif self.direction == "up":
-                self.offset[1] += self.speed*dt
-            elif self.direction == "down":
-                self.offset[1] -= self.speed*dt
+
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            self.offset[1] += self.speed * dt
+            self.blit_offset[1] += self.speed * dt
+        elif keys[pg.K_s]:
+            self.offset[1] -= self.speed * dt
+            self.blit_offset[1] -= self.speed * dt
+        elif keys[pg.K_a]:
+            self.offset[0] += self.speed * dt
+            self.blit_offset[0] += self.speed * dt
+        elif keys[pg.K_d]:
+            self.offset[0] -= self.speed * dt
+            self.blit_offset[0] -= self.speed * dt
--- Scripts/game.py
+++ Scripts/game.py
@@ -17,30 +17,16 @@ class Game:
     def loop(self):
         while self.running:
             dt = self.clock.tick(120)/1000
+            #print(self.clock.get_fps())
+            self.screen.fill((0,0,0))
 
-            self.map.draw()
+            self.map.draw(self.scale_factor)
             self.map.move(dt)
 
             pg.display.flip()
             for event in pg.event.get():
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_w:
-                        self.map.should_move = True
-                        self.map.direction = "up"
-                    elif event.key == pg.K_s:
-                        self.map.should_move = True
-                        self.map.direction = "down"
-                    elif event.key == pg.K_a:
-                        self.map.should_move = True
-                        self.map.direction = "left"
-                    elif event.key == pg.K_d:
-                        self.map.should_move = True
-                        self.map.direction = "right"
-                elif event.type == pg.KEYUP:
-                    if event.key == pg.K_w or event.key == pg.K_s or event.key == pg.K_a or event.key == pg.K_d:
-                        self.map.should_move = False
 
-                elif event.type == pg.QUIT:
+                if event.type == pg.QUIT:
                     self.running = False
                     pg.quit()
 
